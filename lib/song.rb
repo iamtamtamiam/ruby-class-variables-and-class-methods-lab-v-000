015117befa310ee7b5ceaf3a0f53e305298be952
Tam Vu <vutam_a@hotmail.com>
2019-06-03 20:24:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,9 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count
+      if genre_count[genre]
+        genre += 1 
+      else 
     end  
      
   end 
