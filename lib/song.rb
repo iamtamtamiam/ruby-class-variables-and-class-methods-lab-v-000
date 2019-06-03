c6197bb58001492d012873d4589c9b8c5dbc5536
Tam Vu <vutam_a@hotmail.com>
2019-06-03 20:02:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,15 @@ class Song
   
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@genre = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre 
     @@count += 1 
-    if @@genre.include?(genre) == false
-      @@genre << genre 
+    if @@genres.include?(genre) == false
+      @@genres << genre 
     end 
   end
   
@@ -19,7 +19,7 @@ class Song
   end 
 
   def self.genres
-    @@genre
+    @@genres
   end 
   
 end
